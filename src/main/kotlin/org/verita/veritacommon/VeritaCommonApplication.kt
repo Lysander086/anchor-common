f02f044fbff979196082b482c8d02a4aeed6e795
Lysander <19286863+Lysander686@users.noreply.github.com>
2022-12-13 00:05:25 +0900

    init

---
+++ src/main/kotlin/org/verita/veritacommon/VeritaCommonApplication.kt
@@ -0,0 +1,11 @@
+package org.verita.veritacommon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VeritaCommonApplication
+
+fun main(args: Array<String>) {
+	runApplication<VeritaCommonApplication>(*args)
+}
---
+++ src/test/kotlin/org/verita/veritacommon/VeritaCommonApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.verita.veritacommon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VeritaCommonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
